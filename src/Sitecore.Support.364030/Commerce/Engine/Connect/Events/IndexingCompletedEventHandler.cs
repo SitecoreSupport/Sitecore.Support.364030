40ec35e88da4ecae40e47fa6c30aa137d5ba996b
mao <mao@sitecore.net>
2019-10-09 11:29:41 +0300

    Commit default implementation

---
+++ src/Sitecore.Support.364030/Commerce/Engine/Connect/Events/IndexingCompletedEventHandler.cs
@@ -0,0 +1,123 @@
+﻿using Sitecore.Caching;
+using Sitecore.Commerce.Engine.Connect.DataProvider;
+using Sitecore.Commerce.Engine.Connect.Events;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.StringExtensions;
+using Sitecore.Web;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.Events
+{
+    public class IndexingCompletedEventHandler : Sitecore.Commerce.Engine.Connect.Events.IndexingCompletedEventHandler
+    {
+        public override void OnIndexingCompleted(object sender, EventArgs e)
+        {
+            IndexingCompletedEventArgs indexingCompletedEventArgs;
+            if (e == null || (indexingCompletedEventArgs = (e as IndexingCompletedEventArgs)) == null)
+            {
+                return;
+            }
+            Database database = Factory.GetDatabase(indexingCompletedEventArgs.DatabaseName, assert: false);
+            if (database == null)
+            {
+                return;
+            }
+            bool flag = false;
+            Log.Info("OnIndexingCompleted - Started for '" + database.Name + "'.", this);
+            if (indexingCompletedEventArgs.SitecoreIds.Length <= 20)
+            {
+                Log.Info("OnIndexingCompleted - Performing incremental cache updates.", this);
+                string[] sitecoreIds = indexingCompletedEventArgs.SitecoreIds;
+                foreach (string text in sitecoreIds)
+                {
+                    ID result;
+                    if (!ID.TryParse(text, out result))
+                    {
+                        continue;
+                    }
+                    Item item = database.GetItem(result);
+                    if (item != null)
+                    {
+                        Log.Info(string.Format("{0} - Removing '{1}' in database:{2} from caches.", "OnIndexingCompleted", result, database.Name), this);
+                        CatalogRepository.DefaultCache.RemovePrefix(text);
+                        database.Caches.ItemCache.RemoveItem(result);
+                        database.Caches.DataCache.RemoveItemInformation(result);
+                        database.Caches.StandardValuesCache.RemoveKeysContaining(result.ToString());
+                        database.Caches.PathCache.RemoveKeysContaining(result.ToString());
+                        database.Caches.ItemPathsCache.Remove(new ItemPathCacheKey(item.Paths.FullPath, result));
+                        SiteInfo site = GetSite(item);
+                        if (site != null)
+                        {
+                            Log.Info("Using Host name '" + site.HostName + "' with Site '" + site.Name + "' for HTML cache selective refresh", this);
+                            HtmlCache htmlCache = site.HtmlCache;
+                            if (htmlCache != null)
+                            {
+                                htmlCache.RemoveKeysContaining(item.Name);
+                                htmlCache.RemoveKeysContaining(text);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Log.Info(string.Format("{0} - Found new item '{1}'.", "OnIndexingCompleted", result), this);
+                        flag = true;
+                    }
+                }
+            }
+            else
+            {
+                Log.Info("OnIndexingCompleted - Performing full cache refresh.", this);
+                flag = true;
+            }
+            if (flag || IsStandaloneEnvironment())
+            {
+                Log.Info("OnIndexingCompleted - Clear caches.", this);
+                CacheManager.ClearAllCaches();
+                Log.Info("OnIndexingCompleted - Updating mapping entries.", this);
+                new CatalogRepository().UpdateMappingEntries(DateTime.UtcNow);
+            }
+        }
+
+        private SiteInfo GetSite(Item item)
+        {
+            List<SiteInfo> siteInfoList = Factory.GetSiteInfoList();
+            SiteInfo result = null;
+            try
+            {
+                foreach (SiteInfo item2 in siteInfoList)
+                {
+                    string value = "{0}{1}".FormatWith(item2.RootPath, item2.StartItem);
+                    if (!string.IsNullOrWhiteSpace(value) && item.Paths.FullPath.StartsWith(value, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        result = item2;
+                        return result;
+                    }
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                Log.Warn($"Indexing Complete Event Handler - Cannot get SiteInfo for item {item.Name}:{item.ID}", this);
+                return result;
+            }
+        }
+
+        private bool IsStandaloneEnvironment()
+        {
+            string text = ConfigurationManager.AppSettings["role:define"];
+            if (!string.IsNullOrEmpty(text))
+            {
+                return text.Equals(SitecoreRole.Standalone.Name, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+    }
+}
